6149f3e0ca5385b1a611cf9d2da24b6c0debec1f
ANDRES YOVINO LUONI OLIVARES <andres.luoni@inacapmail.cl>
2023-07-10 18:20:31 -0400

    añadimos rutas y componentes

---
+++ src/components/HolaMundo.jsx
@@ -0,0 +1,16 @@
+/* react core*/
+
+import React, {useState,useEffect} from "react"
+
+/*view*/
+const HolaMundo = () => {
+
+    return (
+        <>
+        <h1>hola mundoxd</h1>
+        {/*wenos comentarios xd*/}
+        </>
+    )
+}
+
+export default HolaMundo
